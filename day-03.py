a5c4d192c735d0a11ecd020c7fa1a35b9bce58ea
Sav Bell <savrosebell@gmail.com>
2022-12-02 23:03:50 -0800

    Adding Day 3

---
+++ day-03.py
@@ -0,0 +1,19 @@
+def findPriority(item):
+    if item.islower():
+        return ord(item)-96
+    else:
+        return ord(item)-38
+
+with open('./day-03.txt') as file:
+    rucksacks = file.read().split('\n')
+
+# Part 1
+sharedItems = [set(x).intersection(y).pop() for x, y in [[i[:len(i)//2], i[len(i)//2:]] for i in rucksacks]]
+priorityTotal = sum([findPriority(x) for x in sharedItems])
+print(priorityTotal)
+
+# Part 2
+priorityTotal = 0
+for i in range(0,len(rucksacks),3):
+    priorityTotal += findPriority(set(rucksacks[i]).intersection(rucksacks[i+1]).intersection(rucksacks[i+2]).pop())
+print(priorityTotal)
