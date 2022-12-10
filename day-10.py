efdd41f4f3b6014e9ecb9cbf7f19dff69570fa14
Sav Bell <savrosebell@gmail.com>
2022-12-10 14:20:04 -0800

    Adding Day 10 Part 2 OG solution

--- day-10.py
+++ day-10.py
@@ -21,7 +21,41 @@ for c in cmds:
         cycle = increaseCycle(cycle, 1)
 print('Day 10 Part 1:', sum(signalStrengths))
 
-# Part 2 TBD
+
+# Part 2
+def moveSprite(x):
+    sprite = ['.'] * 40
+    for i in range(x-1,x+2):
+        if i < 40 and i >= 0:
+            sprite[i] = '#'
+    return sprite
+
+def nextCycle():
+    if sprite[cycle-((cycle//40)*40)-1] == '#':
+        nextPixel = '#'
+    else:
+        nextPixel = '.'
+    crt.append(nextPixel)
+
+x, cycle = 1, 0
+sprite = ['#'] * 3 + ['.'] * 37
+crt = []
+for c in cmds:
+    if c[0] == 'addx':
+        for _ in range(2):
+            cycle += 1
+            nextCycle()
+        x += int(c[1])
+        sprite = moveSprite(x)
+    elif c[0] == 'noop':
+        cycle += 1
+        nextCycle()
+
+print('Day 10 Part 2:')
+for i, p in enumerate(crt):
+    print(p,end='')
+    if (i+1) % 40 == 0:
+        print('\n',end='')
 
 # One-line Solutions
 print('Day 10 Part 1:', sum(signalStrengths) if not (signalStrengths := []) and (x := 1) and not (cycle := 0) and [[(c[0] == 'addx') and [(cycle := cycle+1) and ((cycle+20) % 40 == 0 and signalStrengths.append(cycle*x)) for _ in range(2)] and (x := x+int(c[1]))] and [(c[0] == 'noop') and [(cycle := cycle+1) and ((cycle+20) % 40 == 0 and signalStrengths.append(cycle*x))]] for c in [y.split(' ') for y in open('inputs/day-10.txt').read().split('\n')]] else '')
