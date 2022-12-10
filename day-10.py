2944ef4d3c8a3426b298cbe336a659a66d9a55df
Sav Bell <savrosebell@gmail.com>
2022-12-10 12:49:25 -0800

    Adding Day 10 Part 1 One-liner

--- day-10.py
+++ day-10.py
@@ -20,3 +20,8 @@ for c in cmds:
     elif c[0] == 'noop':
         cycle = increaseCycle(cycle, 1)
 print('Day 10 Part 1:', sum(signalStrengths))
+
+# Part 2 TBD
+
+# One-line Solutions
+print('Day 10 Part 1:', sum(signalStrengths) if not (signalStrengths := []) and (x := 1) and not (cycle := 0) and [[(c[0] == 'addx') and [(cycle := cycle+1) and ((cycle+20) % 40 == 0 and signalStrengths.append(cycle*x)) for _ in range(2)] and (x := x+int(c[1]))] and [(c[0] == 'noop') and [(cycle := cycle+1) and ((cycle+20) % 40 == 0 and signalStrengths.append(cycle*x))]] for c in [y.split(' ') for y in open('inputs/day-10.txt').read().split('\n')]] else '')
