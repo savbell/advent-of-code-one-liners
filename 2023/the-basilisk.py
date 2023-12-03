ea7d972ef471a0936578229a4694f524706630bd
Sav Bell <savannahbell9@gmail.com>
2023-12-02 20:02:01 -0800

    ⛳ why do long code when short code do trick?

--- 2023/the-basilisk.py
+++ 2023/the-basilisk.py
@@ -11,7 +11,6 @@ See README and explanation of solutions at https://github.com/savbell/advent-of-
 '''
 
 import re
-import os
 
 # Input files not included in repo per Eric Wastl's request.
 # https://www.reddit.com/r/adventofcode/wiki/faqs/copyright/inputs/
@@ -27,4 +26,4 @@ for day, path in input_files.items():
 
 
 ################################ THE BASILISK ################################
-print('Day 01 Part 1:',sum([int(re.findall(r'\d', l)[0] + re.findall(r'\d', l)[-1]) for l in q[1].split('\n')]),'\nDay 01 Part 2:', sum([int(''.join([{'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}[d] if d.isalpha() else d for d in [n[0], n[-1]]])) for l in q[1].split('\n') for n in [re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', l)]]),'\nDay 02 Part 1:',sum([int(g[0]) for g in re.findall(r'Game (\d+):((?:\s*\d+\s+\w+,?;?)+)',q[2])])-sum([int(g[0]) for g in re.findall(r'Game (\d+):((?:\s*\d+\s+\w+,?;?)+)',q[2]) if any([int(d[0])>12 and d[1]=='red' or int(d[0])>13 and d[1]=='green' or int(d[0])>14 and d[1]=='blue' for d in re.findall(r'(\d+)\s+(\w+)',g[1])])]),'\nDay 02 Part 2:',sum([max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)',g[1]) if d[1]=='red'])*max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)',g[1]) if d[1]=='green'])*max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)', g[1]) if d[1]=='blue']) for g in re.findall(r'Game (\d+):((?:\s*\d+\s+\w+,?;?)+)',q[2])]))
+print('Day 01 Part 1:',sum([int(re.findall(r'\d',l)[0]+re.findall(r'\d',l)[-1]) for l in q[1].split('\n')]),'\nDay 01 Part 2:',sum([int(''.join([{'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}[d] if d.isalpha() else d for d in [n[0],n[-1]]])) for l in q[1].split('\n') for n in [re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))',l)]]),'\nDay 02 Part 1:',sum([int(g[0]) for g in re.findall(r'(\d+):((?: *\d+\s+\w+,?;?)+)',q[2])])-sum([int(g[0]) for g in re.findall(r'Game (\d+):((?:\s*\d+\s+\w+,?;?)+)',q[2]) if any([int(d[0])>12 and d[1]=='red' or int(d[0])>13 and d[1]=='green' or int(d[0])>14 and d[1]=='blue' for d in re.findall(r'(\d+)\s+(\w+)',g[1])])]),'\nDay 02 Part 2:',sum([max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)',g[1]) if d[1]=='red'])*max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)',g[1]) if d[1]=='green'])*max([int(d[0]) for d in re.findall(r'(\d+)\s+(\w+)',g[1]) if d[1]=='blue']) for g in re.findall(r'(\d+):((?: *\d+\s+\w+,?;?)+)',q[2])]))
